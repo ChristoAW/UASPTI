98d008f14b425e91a655ea93fd6d8157b0b7aef4
cell0s <marcellino.osmon@student.umn.ac.id>
2022-06-02 13:58:27 +0700

    layout 1

--- src/components/modules/ProgressBar.jsx
+++ src/components/modules/ProgressBar.jsx
@@ -6,37 +6,50 @@ export default function ProgressBar() {
 
   return (
     <div className="stats">
-      <p>Hunger</p>{" "}
-      <div className="progress">
-        <div
-          className="progress-bar"
-          role="progressbar"
-          style={{ width: stats.hunger + "%" }}
-        ></div>
+      <div className="row">
+        <div className="col-6">
+          <p>Hunger</p>{" "}
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: stats.hunger + "%" }}
+            ></div>
+          </div>
+        </div>
+        <div className="col-6">
+          <p>Fun</p>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: stats.fun + "%" }}
+            ></div>
+          </div>
+        </div>
       </div>
-      <p>Fun</p>
-      <div className="progress">
-        <div
-          className="progress-bar"
-          role="progressbar"
-          style={{ width: stats.fun + "%" }}
-        ></div>
-      </div>
-      <p>Energy</p>
-      <div className="progress">
-        <div
-          className="progress-bar"
-          role="progressbar"
-          style={{ width: stats.energy + "%" }}
-        ></div>
-      </div>
-      <p>Intelligence</p>
-      <div className="progress">
-        <div
-          className="progress-bar"
-          role="progressbar"
-          style={{ width: stats.intel + "%" }}
-        ></div>
+      <div className="row">
+        <div className="col-6">
+        <p>Energy</p>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: stats.energy + "%" }}
+              ></div>
+            </div>
+        </div>
+
+        <div className="col-6">
+        <p>Intelligence</p>
+            <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: stats.intel + "%" }}
+            ></div>
+          </div>
+        </div>
       </div>
       <p>Money: Rp.{stats.money}</p>
     </div>
